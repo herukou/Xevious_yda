ddb1e9f2e0f976942d3566486ed6dcc2ab03e75c
key <mkfxpkrsh@gmail.com>
2017-12-22 13:06:02 +0900

    enemy作成
    敵がバキュラに当たる不具合

---
+++ Assets/script/enemy/Bacula.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bacula : MonoBehaviour {
+
+    Bacula_k Bacura_k;
+
+    void Start()
+    {
+        Bacura_k = GetComponent<Bacula_k>();
+
+        Bacura_k.Move(transform.up * -1);
+    }
+}
---
+++ Assets/script/enemy/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Ship ship;
+
+    void Start()
+    {
+        ship = GetComponent<Ship>();
+
+        ship.Move(transform.up * -1);
+    }
+
+    public void OnTriggerEnter2D(Collider2D c)
+    {
+        ship.Explosion();
+
+        Destroy(gameObject);
+    }
+}
