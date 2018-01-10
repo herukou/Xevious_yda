2307b768e892550801fd7948800e28cdb1f320be
kirito <skiri.yda@gmail.com>
2018-01-10 16:48:17 +0900

    敵基盤　弾発射　追尾未完
    敵の表示と弾発射。しかし、弾が自機を認識できない

---
+++ Assets/script/enemy/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    // 弾を撃つ間隔
+    public float shotDelay;
+
+    // 弾のPrefab
+    public GameObject bullet;
+
+
+    // 弾の作成
+    public void Shot(Transform origin)
+    {
+        Instantiate(bullet, origin.position, origin.rotation);
+    }
+
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        while (true)
+        {
+
+            // 子要素を全て取得する
+            for (int i = 0; i < transform.childCount; i++)
+            {
+
+                Transform shotPosition = transform.GetChild(i);
+
+                // ShotPositionの位置/角度で弾を撃つ
+                Shot(shotPosition);
+            }
+
+            // shotDelay秒待つ
+            yield return new WaitForSeconds(shotDelay);
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/script/enemy/EnemyMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+
+    // 速度
+    public Vector2 speed = new Vector2(0.05f, 0.05f);
+    // ターゲットとなるオブジェクト
+    public GameObject targetObject;
+    // ラジアン変数
+    private float rad;
+    // 現在位置を代入する為の変数
+    private Vector2 Position;
+
+    // Use this for initialization
+    void Start () {
+
+        //player取得
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        // ラジアン
+        rad = Mathf.Atan2(
+            targetObject.transform.position.y - transform.position.y,
+            targetObject.transform.position.x - transform.position.x);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // 現在位置をPositionに代入
+        Position = transform.position;
+        Position.x += speed.x * Mathf.Cos(rad);
+        Position.y += speed.y * Mathf.Sin(rad);
+        // 現在の位置に加算減算を行ったPositionを代入する
+        transform.position = Position;
+    }
+}
