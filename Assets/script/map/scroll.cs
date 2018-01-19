4c3f8c6220723431e96b022b6bd43751a8aaf382
saku1130 <saku0113.s@gmail.com>
2018-01-19 11:49:14 +0900

    ループ完成
    マップと森のBGMができた。次は森完成させる

---
+++ Assets/script/map/scroll.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour {
+
+    [SerializeField]
+    public float moveSpeed;
+    [SerializeField]
+    private float this_x;
+
+    private Vector3 startPosition;
+    public float border;
+   // public float border2;
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(0, +moveSpeed, 0);
+
+        Vector3 pos = transform.position;
+        pos.z -= Time.deltaTime * 1;
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+
+        // （ポイント）zの値が境界線以下になったら初期位置に戻す。
+        if (pos.z <= border)
+        {
+            transform.position = startPosition;
+        }
+       /* if (pos.z <= border2)
+        {
+            transform.position = startPosition;
+
+        }
+        /*  int x=0,y=0;
+            while(true)
+            {
+
+
+             break;
+          }*/
+    }
+}
