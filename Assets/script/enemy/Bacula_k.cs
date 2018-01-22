301a3f08e3b81fd28cf27895df8f7b59c43af554
kirito <skiri.yda@gmail.com>
2018-01-22 10:11:17 +0900

    マージ後のバグ除去後の作業コピー
    マージしたらバグが起きたので元に戻したやつ

---
+++ Assets/script/enemy/Bacula_k.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bacula_k : MonoBehaviour
+{
+
+    //スピード
+    public float speed;
+
+    public void Move(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+
+}
+
---
+++ Assets/script/enemy/Ship.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ship : MonoBehaviour {
+
+    //スピード
+    public float speed;
+
+    //間隔
+    public float shotDelay;
+
+    //弾
+    public GameObject bullet;
+
+    //爆発
+    public GameObject explosion;
+
+
+    //爆発作成
+    public void Explosion()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+
+
+    public void Shot (Transform origin)
+    {
+        Instantiate(bullet, origin.position, origin.rotation);
+    }
+
+
+    public void Move(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+
+}
